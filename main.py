bf8a11c8faa631afcad1c55c11f456d73150860c
ibmnandkishor <121294572+ibmnandkishor@users.noreply.github.com>
2023-11-14 12:35:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,6 @@ if st.sidebar.button("load dataset"):
 if st.sidebar.button("load description"):  
  st.write(df.describe())
 
-if st.sidebar.button("load description"):  
- st.write(df.describe())
 
 #plot
 #a1=pd.DataFrame(df['Year'],df['TotalPrice'])
